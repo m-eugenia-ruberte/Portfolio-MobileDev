6f63abd78287e3e10c2c5a18a003bd818c52c4b6
m-eugenia-ruberte <m.eugenia.ruberte@gmail.com>
2025-08-04 10:53:21 -0700

    Create respuestas.kt

---
+++ 01-kotlin-basics/04 - Bucles/respuestas.kt
@@ -0,0 +1,26 @@
+fun main() {
+    println("Ejercicio 1 - Contador del 1 al 10")
+    // TODO: Completa aquí el bucle for para imprimir del 1 al 10
+
+    println("\nEjercicio 2 - Suma de números del 1 al 100")
+    // TODO: Completa aquí el bucle while para sumar del 1 al 100
+
+    println("\nEjercicio 3 - Bucle con condición de salida")
+    // TODO: Completa aquí el bucle do-while para pedir palabras hasta "salir"
+
+    println("\nEjercicio 4 - Números pares del 1 al 20")
+    // TODO: Completa aquí para imprimir números pares usando continue
+
+    println("\nEjercicio 5 - Tabla de multiplicar del 5")
+    // TODO: Completa aquí el bucle for para mostrar la tabla del 5
+}
+
+// Ejercicio 1
+
+// Ejercicio 2
+
+// Ejercicio 3
+
+// Ejercicio 4
+
+// Ejercicio 5
