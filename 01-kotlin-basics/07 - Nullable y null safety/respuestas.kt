19e89d8c031d73b6bd6ff428bd02af3516b4f27d
m-eugenia-ruberte <m.eugenia.ruberte@gmail.com>
2025-08-05 10:26:38 -0700

    Update respuestas.kt

--- 01-kotlin-basics/07 - Nullable y null safety/respuestas.kt
+++ 01-kotlin-basics/07 - Nullable y null safety/respuestas.kt
@@ -4,40 +4,66 @@ fun main() {
 
     // Ejercicio 1: Variable nullable y operador seguro
     println("Ejercicio 1:")
-    // Aquí va el código de prueba
+    val variable:Int? = null
+    println(variable?:2)
     println()
 
     // Ejercicio 2: Función con operador Elvis
     println("Ejercicio 2:")
-    // Aquí va el código de prueba
+    
+    longitudTexto(null)
+    
+    longitudTexto("no se que poner")
     println()
 
-    // Ejercicio 3: Safe cast con as?
+    // Ejercicio 3: Uso de let con variable nullable
     println("Ejercicio 3:")
-    // Aquí va el código de prueba
+    
+    imprimirTexto(null)
+    imprimirTexto("Hola")
+    
     println()
 
-    // Ejercicio 4: Uso de let con variable nullable
+    // Ejercicio 4: Safe cast con as?
     println("Ejercicio 4:")
-    // Aquí va el código de prueba
+    imprimirAny(null)
+    imprimirAny("Hola")
+    imprimirAny(2)
+    imprimirAny(true)
+    
     println()
 
     // Ejercicio 5: Operador !! explicado en comentarios
     println("Ejercicio 5:")
-    // Aquí va el código de prueba
-    println()
+    var texto:String?= "Sólo se puede usar !! cuando estas seguro que no sera nulo"
+    println(texto + " " + texto!!.length)
+    texto = null
+    println(texto!!.length)
+    
 }
 
 // Ejercicio 1
 
 
 // Ejercicio 2
-
+fun longitudTexto(texto:String?){
+    println(texto?.length)
+}
 
 // Ejercicio 3
 
-
+fun imprimirTexto(texto:String?){
+    texto?.let{
+       println(it) 
+    }
+}
 // Ejercicio 4
-
+fun imprimirAny(valor:Any?){
+    if (valor != null) {
+        println("Valor: $valor")
+    } else {
+        println("Es null")
+    }
+}
 
 // Ejercicio 5
