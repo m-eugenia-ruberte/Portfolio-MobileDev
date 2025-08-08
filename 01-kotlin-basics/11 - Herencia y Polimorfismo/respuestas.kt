1605dd7ae41073b71a3b7d4f40cf61465200b176
m-eugenia-ruberte <m.eugenia.ruberte@gmail.com>
2025-08-08 03:27:01 -0700

    Create respuestas.kt

---
+++ 01-kotlin-basics/11 - Herencia y Polimorfismo/respuestas.kt
@@ -0,0 +1,39 @@
+fun main() {
+    println("Respuestas a los ejercicios de Herencia y Polimorfismo")
+    println("=====================================================\n")
+
+    // Ejercicio 1: Herencia básica
+    println("Ejercicio 1:")
+    
+    println()
+
+    // Ejercicio 2: Polimorfismo
+    println("Ejercicio 2:")
+    
+    println()
+
+    // Ejercicio 3: Clases abstractas
+    println("Ejercicio 3:")
+    
+    println()
+
+    // Ejercicio 4: Interfaces
+    println("Ejercicio 4:")
+    
+    println()
+
+    // Ejercicio 5: Uso combinado
+    println("Ejercicio 5:")
+    
+    println()
+}
+
+// Ejercicio 1
+
+// Ejercicio 2
+
+// Ejercicio 3
+
+// Ejercicio 4
+
+// Ejercicio 5
