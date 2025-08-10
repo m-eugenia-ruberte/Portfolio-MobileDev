4017726b8da68acc7fa4c1de73674cfe482fb6a3
m-eugenia-ruberte <m.eugenia.ruberte@gmail.com>
2025-08-10 10:24:45 -0700

    Create respuestas.kt

---
+++ 01-kotlin-basics/12 - Extensiones y expresiones infix/respuestas.kt
@@ -0,0 +1,43 @@
+fun main() {
+    println("Respuestas a los ejercicios de Extensiones y expresiones infix")
+    println("================================================================\n")
+
+    // Ejercicio 1: Función de extensión simple
+    println("Ejercicio 1:")
+    
+    println()
+
+    // Ejercicio 2: Extensión con lógica condicional
+    println("Ejercicio 2:")
+    
+    println()
+
+    // Ejercicio 3: Extensión sobre colecciones
+    println("Ejercicio 3:")
+    
+    println()
+
+    // Ejercicio 4: Función infix personalizada
+    println("Ejercicio 4:")
+    
+    println()
+
+    // Ejercicio 5: Combinar infix con otras funciones
+    println("Ejercicio 5:")
+    
+    println()
+}
+
+// Ejercicio 1
+
+
+// Ejercicio 2
+
+
+// Ejercicio 3
+
+
+// Ejercicio 4
+
+
+// Ejercicio 5
