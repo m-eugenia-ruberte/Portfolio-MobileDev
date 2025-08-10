2baaa874e87d54252bfe7a04ad19ccc0686b7289
m-eugenia-ruberte <m.eugenia.ruberte@gmail.com>
2025-08-10 11:04:32 -0700

    Create respuestas.kt

---
+++ 01-kotlin-basics/13 - Introducción a Coroutines (muy básico)/respuestas.kt
@@ -0,0 +1,36 @@
+fun main() {
+    println("Respuestas a los ejercicios de Introducción a Coroutines")
+    println("======================================================\n")
+
+    // Ejercicio 1 - Lanzar coroutine simple
+    println("Ejercicio 1:")
+    
+    println()
+
+    // Ejercicio 2 - Función suspend simple
+    println("Ejercicio 2:")
+    
+    println()
+
+    // Ejercicio 3 - Uso de async y await
+    println("Ejercicio 3:")
+    
+    println()
+
+    // Ejercicio 4 - Dispatcher y contexto
+    println("Ejercicio 4:")
+    
+    println()
+}
+
+// Ejercicio 1
+
+
+// Ejercicio 2
+
+
+// Ejercicio 3
+
+
+// Ejercicio 4
+
