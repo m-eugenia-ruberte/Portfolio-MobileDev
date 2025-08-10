3888220712cdcd625df997912d83b2ee63bbd4f8
m-eugenia-ruberte <m.eugenia.ruberte@gmail.com>
2025-08-10 03:56:45 -0700

    Create respuestas.kt

---
+++ 01-kotlin-basics/12 - Manejo de errores (try_catch)/respuestas.kt
@@ -0,0 +1,43 @@
+fun main() {
+    println("Respuestas a los ejercicios de Manejo de errores (try/catch)")
+    println("================================================================\n")
+
+    // Ejercicio 1: Conversión segura de String a Int
+    println("Ejercicio 1:")
+    
+    println()
+
+    // Ejercicio 2: División con control de errores
+    println("Ejercicio 2:")
+    
+    println()
+
+    // Ejercicio 3: Uso de finally
+    println("Ejercicio 3:")
+    
+    println()
+
+    // Ejercicio 4: Excepción personalizada
+    println("Ejercicio 4:")
+    
+    println()
+
+    // Ejercicio 5: Múltiples excepciones
+    println("Ejercicio 5:")
+    
+    println()
+}
+
+// Ejercicio 1
+
+
+// Ejercicio 2
+
+
+// Ejercicio 3
+
+
+// Ejercicio 4
+
+
+// Ejercicio 5
