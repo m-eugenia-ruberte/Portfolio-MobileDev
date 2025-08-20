ea4518a53f312478699ad8d48f2bd14002ce4cde
“MEugeniaBraingapps” <meugenia@braingapps.com>
2025-08-20 19:48:32 +0200

    finalizado arrays-en-Kotlin.kt

---
+++ 02-android-developer/01-android-basics-compose/Unidad-3-Cómo mostrar-listas-y-usar-material-Design/01-Más-aspectos-básicos-de-Kotlin/02-Cómo-usar-colecciones-en-Kotlin/arrays-en-Kotlin.kt
@@ -0,0 +1,26 @@
+
+
+fun main() {
+
+    val rockPlanets = arrayOf("Mercury", "Venus", "Earth", "Mars")
+
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+
+    val solarSystem = rockPlanets + gasPlanets
+
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
+
+    solarSystem[3] = "Little Earth"
+    println(solarSystem[3])
+    val newSolarSystem = solarSystem + "Pluto"
+    println(newSolarSystem[8])
+}
+
+
