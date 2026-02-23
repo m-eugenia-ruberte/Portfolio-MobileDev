b87844ff6bf63de262035f61afa098028f68f0e4
“MEugeniaBraingapps” <meugenia@braingapps.com>
2026-02-23 13:15:44 +0100

    trasladar proyecto

--- 03-UrticDiary/UrticDiary/app/src/main/java/com/meruberte/urticdiary/ui/screens/list/EntryListScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.meruberte.urticdiary.ui.screens.list
-
-import androidx.compose.runtime.Composable
-
-
-@Composable
-fun EntryListScreen() {
-    // Lista de registros (en desarrollo)
-}
